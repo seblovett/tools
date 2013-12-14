d73eecbb71139af6e666a296c38233b977a21d6b
seblovett <seblovett@googlemail.com>
2013-12-14 22:07:06 +0000

    Now stores latest ip to memory. If ip changes, it will be resubmitted

--- submitip.sh
+++ submitip.sh
@@ -1,3 +1,21 @@
 ip=$(lynx --dump http://ipecho.net/plain)
 ip=$(echo $ip | tr -d " ")
-lynx --dump http://www.seblovett.co.uk/ip.php?ip=${ip}
+#get old ip from file
+if [ -f "ip.txt" ]
+then
+	echo "ip.txt found."
+	ipold=$(cat ip.txt)
+else
+	#make file if it isn't there
+	echo "ip.txt not found."
+	echo $ip > ip.txt #and put the ip in it
+	ipold="noip"
+fi
+
+if [ "$ip" = "$ipold" ]; then
+	echo "ip not changed"
+else
+	lynx --dump http://www.seblovett.co.uk/ip.php?ip=${ip}
+	#echo "Submitting ip" 
+fi
+echo $ip > ip.txt
