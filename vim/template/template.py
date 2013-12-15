1fcab3afaa7adf551f6026ed0085da854d0fa67e
seblovett <seblovett@googlemail.com>
2013-12-15 21:05:57 +0000

    Added a python template

---
+++ vim/template/template.py
@@ -0,0 +1,14 @@
+# Python Template
+# @author seblovett
+# @brief A brief description of this code
+
+## @brief Documentation for a function.
+#  @param - None
+#  @retval - None
+#  More details.
+def func():
+	pass
+
+if "__main__" == __name__:
+	''' Code to be run if this is main '''
+	pass
