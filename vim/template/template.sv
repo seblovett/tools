4c605736d06873db441d8e1e06ba4c42f3ad7eb0
seblovett <seblovett@googlemail.com>
2014-02-04 20:57:53 +0000

    System verilog template added

---
+++ vim/template/template.sv
@@ -0,0 +1,25 @@
+// <+FILENAME+>
+// Writen by <+AUTHOR+>
+// Date Created <+DATECREATED+>
+// Last Edited: 
+
+
+module <+FILENAME+> (
+	input logic Clock, nReset, D
+	output logic Q, nQ)
+
+always_ff @ (posedge Clock or negedge nReset)
+begin
+	if(!nReset) //Reset the system
+		Q <= 0;
+	else
+	 begin
+		Q <= D;
+	 end
+end
+
+always_comb
+ begin
+	nQ = ~Q;
+ end
+endmodule
